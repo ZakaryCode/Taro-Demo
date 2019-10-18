a3905a4261bbd8f4f8e0a554ed97304113ace0b5
zhutianjian <zakarycode@gmail.com>
2019-10-18 16:20:59 +0800

    feat: silder for delete

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,7 @@
 import Taro, { Component, Config } from '@tarojs/taro'
 import { View, Text, Image } from '@tarojs/components'
 import './index.scss'
+import '../../app.scss'
 
 const goods = [{
   id: 1,
--- src/pages/indexS/index.tsx
+++ src/pages/indexS/index.tsx
@@ -1,6 +1,7 @@
 import Taro, { Component, Config } from '@tarojs/taro'
 import { View, Text, Image } from '@tarojs/components'
 import './index.scss'
+import '../../app.scss'
 
 export default class Index extends Component {
 
@@ -16,7 +17,8 @@ export default class Index extends Component {
   }
 
   state = {
-    shopCart: []
+    shopCart: [],
+    moveAction: false
   }
 
   componentWillMount () {
@@ -62,27 +64,130 @@ export default class Index extends Component {
     });
   }
 
+  onTouchStartHandler = (good) => (event) => {
+    const { pageX } = event.changedTouches[0];
+    const children = event.currentTarget.children;
+    const hideWidth = children[children.length - 1].offsetWidth;
+    let _lists: any[] = this.state.shopCart;
+    _lists.map(_item => {
+      if (_item.id != good.id) {
+        _item.x = 0;
+      } else {
+        _item.startX = pageX;
+        _item.widthRange = hideWidth;
+      }
+    });
+    this.setState({
+      shopCart: _lists
+    });
+  }
+
+  onHandleSlideOut = (good) => (event) => {
+    const { moveAction } = this.state;
+    if (!moveAction) {
+      return;
+    }
+    const { pageX } = event.changedTouches[0];
+    const distance = pageX - good.startX;
+    let _lists: any[] = this.state.shopCart;
+    _lists.forEach(_item => {
+      if (_item.id == good.id) {
+        _item.x = distance < - good.widthRange / 2 ? - good.widthRange : 0;
+        _item.startX = 0;
+      }
+    });
+    this.setState({
+      shopCart: _lists,
+      moveAction: false
+    });
+  }
+
+  onTouchMoveHandler = (good) => (event) => {
+    const { pageX } = event.changedTouches[0];
+    const distance = pageX - good.startX;
+    let _lists: any[] = this.state.shopCart;
+    _lists.forEach(_item => {
+      if (_item.id == good.id) {
+        let x = distance;
+        if (x > 0) x = 0;
+        if (x < - good.widthRange) x = - good.widthRange;
+        _item.x = x;
+        _item.startX = good.startX;
+      }
+    });
+    this.setState({
+      shopCart: _lists,
+      moveAction: true
+    });
+  }
+
+  onDeleteItem = (good) => (event) => {
+    event.stopPropagation();
+    Taro.showModal({
+      title: "提示",
+      content: `确定要从购物车移除“${good.name}”吗？`,
+      success: res => {
+        let _lists: any[] = this.state.shopCart;
+        if (res.confirm) {
+          _lists.map((_item, index, _lists) => {
+            if (_item.id == good.id) {
+              _lists.splice(index, 1);
+            }
+          });
+          this.setState(
+            {
+              lists: _lists
+            },
+            () => {
+              Taro.showToast({
+                title: "删除成功",
+                icon: "success"
+              });
+            }
+          );
+        } else if (res.cancel) {
+          _lists.forEach(_item => {
+            if (_item.id == good.id) {
+              _item.startX = 0;
+              _item.x = 0;
+            }
+          });
+          this.setState({
+            lists: _lists
+          });
+        }
+      }
+    });
+  }
+
   render () {
     const cart: any[] = this.state.shopCart || [];
-    console.log(this.config.navigationBarTitleText)
     return (
       <View className='index'>
         {process.env.TARO_ENV === 'h5' ? <View className='header'>{this.config.navigationBarTitleText}</View> : ''}
         <View className='carts'>
-          {cart.map((e, i) => <View key={e.id} className='good'>
-            <Image className='good_image' src={e.images[0]} />
-            <View className='good_info'>
-              <Text className='good_info_name text_line_2'>{e.name}</Text>
-              <View className='good_info_bottom'>
-                <Text className='good_info_bottom_price'>¥{(e.price / 100).toFixed(2)}</Text>
-                <View className='good_info_bottom_counters'>
-                  <View className='bg-icon good_info_bottom_counters_controller' onClick={this.handleCartCounters(false, e)}>-</View>
-                  <View className='good_info_bottom_counters_amount' >{e.count}</View>
-                  <View className='bg-icon good_info_bottom_counters_controller' onClick={this.handleCartCounters(true, e)}>+</View>
+          {cart.map((e, i) => 
+            <View key={e.id} className='cart_one'>
+              <View className='good cart_one_good'
+                  onTouchStart={this.onTouchStartHandler(e)}
+                  onTouchEnd={this.onHandleSlideOut(e)}
+                  onTouchMove={this.onTouchMoveHandler(e)}
+                  style={{ left: e.x || 0 }}>
+                <Image className='good_image' src={e.images[0]} />
+                <View className='good_info'>
+                  <Text className='good_info_name text_line_2'>{e.name}</Text>
+                  <View className='good_info_bottom'>
+                    <Text className='good_info_bottom_price'>¥{(e.price / 100).toFixed(2)}</Text>
+                    <View className='good_info_bottom_counters'>
+                      <View className='bg-icon good_info_bottom_counters_controller' onClick={this.handleCartCounters(false, e)}>-</View>
+                      <View className='good_info_bottom_counters_amount' >{e.count}</View>
+                      <View className='bg-icon good_info_bottom_counters_controller' onClick={this.handleCartCounters(true, e)}>+</View>
+                    </View>
+                  </View>
                 </View>
+                <View className='good_delete' onClick={this.onDeleteItem(e)}>删除</View>
               </View>
-            </View>
-          </View>)}
+            </View>)}
         </View>
         <View className={`settle-accounts-btn settle-accounts-btn_${cart.length < 1 ? 'empty' : 'shop'}`} onClick={() => cart.length < 1 ?
             Taro.showToast({ title: '购物车为空', icon: 'none', duration: 2000 }) :
